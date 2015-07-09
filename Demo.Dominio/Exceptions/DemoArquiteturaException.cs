c47ef59b496a1963e40dfe99f163c82817384b8e
Waldyr Felix <waldyrfelix@gmail.com>
2015-07-09 19:58:15 -0300

    removidos arquivos desnecessários

---
+++ Demo.Dominio/Exceptions/DemoArquiteturaException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo.Dominio.Exceptions
+{
+    public class DemoArquiteturaException : Exception
+    {
+        public DemoArquiteturaException(string mensagem)
+            : base(mensagem) { }
+
+        public DemoArquiteturaException(string mensagem, Exception exception)
+            : base(mensagem, exception) { }
+    }
+}
